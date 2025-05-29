cff84382a0651ca336ee8f32c8ddc2407af470fd
stefan.trippler <stefan.trippler@betterask.erni>
2025-05-29 08:12:31 +0200

    LVGL demo added

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,66 @@
 #include <inttypes.h>
 #endif
 
+
+/**************************LVGL and UI************************
+if you want to use the LVGL demo. you need to include <demos/lv_demos.h> and <examples/lv_examples.h>. 
+if not, please do not include it. It will waste your Flash space.
+**************************************************************/
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+#include <examples/lv_examples.h>
+/**************************LVGL and UI END************************/
+
+
 #include "gfx_conf.h"
 
 
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t disp_draw_buf1[screenWidth * screenHeight / 10];
+static lv_color_t disp_draw_buf2[screenWidth * screenHeight / 10];
+static lv_disp_drv_t disp_drv;
+
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+   uint32_t w = ( area->x2 - area->x1 + 1 );
+   uint32_t h = ( area->y2 - area->y1 + 1 );
+
+   tft.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);
+
+   lv_disp_flush_ready( disp );
+
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+   uint16_t touchX, touchY;
+   bool touched = tft.getTouch( &touchX, &touchY);
+   if( !touched )
+   {
+      data->state = LV_INDEV_STATE_REL;
+   }
+   else
+   {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touchX;
+      data->point.y = touchY;
+
+      Serial.print( "Data x " );
+      Serial.println( touchX );
+
+      Serial.print( "Data y " );
+      Serial.println( touchY );
+   }
+}
+
+
 void setup()
-{ 
+{
 #ifdef ARDUINO
   Serial.begin(115200);
   Serial.println(APPLICATION_NAME);
@@ -19,42 +74,57 @@ void setup()
   std::cout << __DATE__ " " __TIME__ << std::endl;
 #endif
 
+  //GPIO init
+  pinMode(38, OUTPUT);
+  digitalWrite(38, LOW);
+  pinMode(17, OUTPUT);
+  digitalWrite(17, LOW);
+  pinMode(18, OUTPUT);
+  digitalWrite(18, LOW);
+  pinMode(42, OUTPUT);
+  digitalWrite(42, LOW);
+
+
   //Display Prepare
   tft.begin();
   tft.fillScreen(TFT_BLACK);
-  tft.setTextSize(3);
+  tft.setTextSize(2);
+  delay(200);
+
+  lv_init();
+
   delay(100);
 
-  tft.fillScreen(TFT_BLUE);
-  delay(1000);
-  tft.fillScreen(TFT_YELLOW);
-  delay(1000);
-  tft.fillScreen(TFT_GREEN);
-  delay(1000);
-  tft.fillScreen(TFT_WHITE);
-  delay(1000);
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf1, disp_draw_buf2, screenWidth * screenHeight/10);
+  /* Initialize the display */
+  lv_disp_drv_init(&disp_drv);
+  /* Change the following line to your display resolution */
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.full_refresh = 1;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  /* Initialize the (dummy) input device driver */
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
   tft.fillScreen(TFT_BLACK);
-  tft.fillCircle    ( 100, 100      , 50, TFT_YELLOW);
-  tft.setCursor(200, 240);
-  tft.print("Hello, Elecrow");
-  Serial.println( "Hello, my Display" );
+
+  lv_demo_widgets();    // LVGL demo
+  
+  Serial.println( "Setup done" );
+
 }
-
-
 
 void loop()
 {
-  uint16_t touchX, touchY;
-  bool touched = tft.getTouch( &touchX, &touchY);
-  if( touched )
-  {
-    tft.fillCircle(touchX, touchY, 10, TFT_WHITE);
-    Serial.print( "Data x " );
-    Serial.println( touchX );
-
-    Serial.print( "Data y " );
-    Serial.println( touchY );
-  }
+    lv_timer_handler();
+    delay(5);
 }
 
 
