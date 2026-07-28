f8f45fba8757ad162a500cc457c8200eef727a5e
stefan.trippler <stefan.trippler@betterask.erni>
2026-07-28 14:05:53 +0200

    version number added

--- hal/main_freertos/main.cpp
+++ hal/main_freertos/main.cpp
@@ -16,7 +16,7 @@ Controller g_controller(g_model, g_view);
 
 int main(int argc, char *argv[])
 {
-    std::cout << APPLICATION_NAME << std::endl;
+    std::cout << APPLICATION_NAME << " " << APPLICATION_VERSION << std::endl;
     std::cout << __DATE__ " " __TIME__ << std::endl;
 
     g_controller.setup();
